c91518e6286b8d19d9290e0f6a0ea93d624904e3
unknown <saralaswarna.s@gmail.com>
2021-06-01 14:50:23 +0530

    catapult added

--- sketch.js
+++ sketch.js
@@ -36,14 +36,20 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
+
+    fill("red")
+    textSize(20);
+    strokeWeight(4);
+    text(mouseX + "," + mouseY ,30,20)
+
     Engine.update(engine);
     strokeWeight(4);
     box1.display();
